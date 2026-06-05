4972c6cb5f6b0979f6cbd0e6fab340e12b21826a
Youm1OuO <511220826@qq.com>
2026-06-05 12:12:12 +0800

    这个版本依旧是使用手来划分时间段的（如果能准确识别的话，这个版本其实效果还不错的，只是增加物品时有点小问题而已），我现在保存这个版本，后续修改成不用手进行划分

--- tools/yolov5/run_train_stage_c.sh
+++ tools/yolov5/run_train_stage_c.sh
@@ -9,3 +9,19 @@ python fridge_project/scripts/train.py stage-c \
     --cache ram \
     --name hand_plus_v2 \
     --freeze 0
+
+
+
+python fridge_project/scripts/train.py stage-c \
+    --data fridge_project/datasets/public_merged/public_merged.yaml \
+    --weights yolov5s.pt \
+    --epochs 600 \
+    --batch-size 32 \
+    --imgsz 640 \
+    --workers 8 \
+    --cache ram \
+    --name hand_plus_v2 \
+    --freeze 0
+
+#   - stage-b：默认用 hyp.scratch-low.yaml（mosaic 1.0），适合从预训练权重开始训                                                                         
+#   - stage-c：默认用 hyp_finetune.yaml（mosaic 0.8），适合微调自己的模型
--- 训练yolov5的指令.sh
+++ 训练yolov5的指令.sh
@@ -61,16 +61,16 @@ python fridge_project/scripts/train.py stage-b \
 
 
 
-python fridge_project/scripts/train.py stage-c \
-    --data    fridge_project/datasets/public_merged/public_merged.yaml \
-    --weights runs/train/stage_b_640_v3_b_clean/weights/best.pt \                                                               
-    --epochs  600 \
-    --batch-size 32 \
-    --imgsz   640 \
-    --workers 8 \
-    --cache   ram \
-    --name    hand_plus_v1 \
-    --freeze  0
+  python fridge_project/scripts/train.py stage-c \
+      --data fridge_project/datasets/public_merged/public_merged.yaml \
+      --weights yolov5s.pt \
+      --epochs 600 \
+      --batch-size 32 \
+      --imgsz 640 \
+      --workers 8 \
+      --cache ram \
+      --name hand_plus_v2 \
+      --freeze 0
 # -------------------------------------------------------------------------------
 
 
